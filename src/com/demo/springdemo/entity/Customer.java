b794ee837731e5d1dd0166fb8e460b760642730f
Suraj Gupta <surajrocks1997@gmail.com>
2022-08-14 16:26:57 +0530

    updated to getMapping from RequestMapping

--- src/com/demo/springdemo/entity/Customer.java
+++ src/com/demo/springdemo/entity/Customer.java
@@ -64,7 +64,4 @@ public class Customer {
 	public String toString() {
 		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
 	}
-	
-	
-	
 }
